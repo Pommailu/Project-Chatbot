249aef5958a4926fa8997b23b174bfdbd34ddc27
Matheepat Pocksubpaiboon <93998287+Pommailu@users.noreply.github.com>
2025-04-24 19:01:38 +0700

    Update Lin chatbot

---
+++ utils/flexTemplates.js
@@ -0,0 +1,390 @@
+const seaTripCards = [
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://www.trip-attractive.com/wp-content/uploads/2017/04/phi-phi-island.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "เกาะพีพี", weight: "bold", size: "xl" },
+          { type: "text", text: "ทะเลน้ำใส ดำน้ำดูปะการัง" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://mediaim.expedia.com/destination/1/a4b30d63cd31089802b028d7708f71e2.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "เกาะเสม็ด", weight: "bold", size: "xl" },
+          { type: "text", text: "หาดทรายขาว น้ำทะเลใสสะอาด" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://www.akiralipe.com/wp-content/uploads/2023/04/%E0%B8%AB%E0%B8%B2%E0%B8%94%E0%B8%8B%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%8B%E0%B9%87%E0%B8%97-1536x863.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "เกาะหลีเป๊ะ", weight: "bold", size: "xl" },
+          { type: "text", text: "มัลดีฟส์เมืองไทย น้ำทะเลใส หาดทรายขาว" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/188000/188439-Patong-Beach.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "หาดป่าตอง", weight: "bold", size: "xl" },
+          { type: "text", text: "หาดยอดฮิตของภูเก็ต ทั้งทะเลสวยและสีสันยามค่ำคืน" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://s359.kapook.com//pagebuilder/9d8242cd-e5d3-473a-a4ce-a340f3207675.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "หาดไร่เลย์", weight: "bold", size: "xl" },
+          { type: "text", text: "หาดสวย เงียบสงบ ปีนผา ดำน้ำ" }
+        ]
+      }
+    }
+  ];
+  
+  const mountainTripCards = [
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://image-tc.galaxy.tf/wijpeg-sxrfid5inslt46adwg0pwpho/intanon_standard.jpg?crop=112%2C0%2C1777%2C1333&width=1140",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "ดอยอินทนนท์", weight: "bold", size: "xl" },
+          { type: "text", text: "ยอดดอยสูงสุดในประเทศไทย" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://s359.kapook.com//pagebuilder/2588c72e-cc8a-4e41-bbe8-14391d22a759.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "ภูทับเบิก", weight: "bold", size: "xl" },
+          { type: "text", text: "ชมทะเลหมอก ทุ่งกะหล่ำปลี อากาศเย็นตลอดปี" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://www.one2go.co/wp-content/uploads/2019/09/3-1568x1045.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "ดอยอ่างขาง", weight: "bold", size: "xl" },
+          { type: "text", text: "สัมผัสอากาศหนาว ชมดอกไม้เมืองหนาว" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://tour.trekkingthai.com/wp-content/uploads/2022/09/%E0%B8%9E%E0%B8%B5%E0%B9%88%E0%B8%9A%E0%B8%AD%E0%B8%AA-768x513.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "ดอยหลวงเชียงดาว", weight: "bold", size: "xl" },
+          { type: "text", text: "ภูเขาหินปูนสูงตระหง่าน จุดชมดาวยอดนิยม" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://mpics.mgronline.com/pics/Images/564000012301201.JPEG",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "ภูเรือ", weight: "bold", size: "xl" },
+          { type: "text", text: "ภูเขารูปร่างเหมือนเรือ อากาศหนาวตลอดปี" }
+        ]
+      }
+    }
+  ];
+  
+  const natureTripCards = [
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://s359.kapook.com//pagebuilder/e670daa6-c52e-40ca-9efa-162946cf5e0d.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "น้ำตกเอราวัณ", weight: "bold", size: "xl" },
+          { type: "text", text: "น้ำตกใสแจ๋วหลายชั้นในป่าเขียวขจี" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://s359.kapook.com//pagebuilder/7fb0976d-5275-4588-8a4e-1144b0af60e1.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "น้ำตกทีลอซู", weight: "bold", size: "xl" },
+          { type: "text", text: "น้ำตกขนาดใหญ่ที่สุดในเมืองไทย" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://cdn-cmmjo.nitrocdn.com/HauDXVjzrGZODVigpbwepJmoTIhWQHlr/assets/static/optimized/rev-9268bbc/wp-content/uploads/2020/09/1581997858511.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "เขื่อนเชี่ยวหลาน", weight: "bold", size: "xl" },
+          { type: "text", text: "ทะเลสาบเหนือเขื่อนท่ามกลางภูเขาหินปูน" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://image-tc.galaxy.tf/wijpeg-bm6qiztzyqyt9hwjhdamzq26j/khao-yai-national-park_standard.jpg?crop=86%2C0%2C1428%2C1071&width=1140",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "เขาใหญ่", weight: "bold", size: "xl" },
+          { type: "text", text: "ผืนป่าเขียวชอุ่ม สวรรค์ของนักดูสัตว์" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://img.kapook.com/u/2019/Tanapol/travel/august/phukadeung/p7.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "อุทยานภูกระดึง", weight: "bold", size: "xl" },
+          { type: "text", text: "ยอดเขาชมวิวสวยงามในฤดูหนาว" }
+        ]
+      }
+    }
+  ];
+  
+  const templeTripCards = [
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://d2e5ushqwiltxm.cloudfront.net/wp-content/uploads/sites/62/2024/08/29075839/bangkok-temple.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "วัดพระแก้ว", weight: "bold", size: "xl" },
+          { type: "text", text: "วัดหลวงงดงามใจกลางกรุงเทพฯ" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://www.novotelbkk.com/wp-content/uploads/sites/62/2016/11/Destination-Temple-of-the-Dawn.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "วัดอรุณ", weight: "bold", size: "xl" },
+          { type: "text", text: "พระปรางค์สูงตระหง่านริมแม่น้ำเจ้าพระยา" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://img.kapook.com/u/pirawan/travel3/Doi%20Suthep5.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "วัดพระธาตุดอยสุเทพ", weight: "bold", size: "xl" },
+          { type: "text", text: "วัดคู่บ้านคู่เมืองเชียงใหม่บนดอยสูง" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://image-tc.galaxy.tf/wijpeg-7pqufmqti0twcdzzq3btsb5lq/wat-rong-khun_standard.jpg?crop=0%2C0%2C555%2C416",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "วัดร่องขุ่น", weight: "bold", size: "xl" },
+          { type: "text", text: "วัดสีขาวศิลปะแห่งเมืองเชียงราย" }
+        ]
+      }
+    },
+    {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://www.angelstartravel.com/backend/images/ContentPhoto/m2CQFdvW2911202315031011.jpg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover"
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          { type: "text", text: "วัดถ้ำเสือ", weight: "bold", size: "xl" },
+          { type: "text", text: "วัดวิวสวยบนเนินเขา จ.กาญจนบุรี" }
+        ]
+      }
+    }
+  ];
+  
+  module.exports = {
+    seaTripCarousel: {
+      type: "carousel",
+      contents: seaTripCards
+    },
+    mountainTripCarousel: {
+      type: "carousel",
+      contents: mountainTripCards
+    },
+    natureTripCarousel: {
+      type: "carousel",
+      contents: natureTripCards
+    },
+    templeTripCarousel: {
+      type: "carousel",
+      contents: templeTripCards
+    }
+  };
